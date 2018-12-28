8fc7ba578dc8b073052989ac5a768b4aee61285d
Antonis Klironomos <antonisklironomos@gmail.com>
2018-12-28 21:36:38 +0200

    Created 2 basic structs

--- utils.h
+++ utils.h
@@ -7,10 +7,18 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+typedef struct INode {
+    struct timespec lastModTime;
+    off_t size;
+    char** names;
+    unsigned int namesNum;
+    struct INode* destINode; // will be null for a destination i-node
+} INode;
+
 typedef struct DirTreeNode {
     char* name;
-    struct stat* iNodeP;
-    struct DirTreeNode* dirTreeNodes;
+    struct INode* iNodeP;
+    struct DirTreeNode* nextNode, *prevNode, *firstChildNode; // covers all different cases by setting null the variables that we don't need
 } DirTreeNode;
 
 #endif
